d775f0ec6f7d0615a8fe5aefd425361e0cde9d95
Vitthal Padwal <vitthalpadwal89@gmail.com>
2017-10-03 19:12:35 +0530

    updated contact pag and database

---
+++ gsm/Models/db_connect.cs
@@ -0,0 +1,118 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gsm.Models
+{
+    public class db_connect
+    {
+        private MySqlConnection connection;
+        public List<string>[] list_feedback_show = new List<string>[3];
+        public List<string>[] list_time_show = new List<string>[1];
+        public List<string>[] list_gallery_show = new List<string>[2];
+
+        private bool OpenConnection()
+        {
+            string connetionString = null;
+            connetionString = "server=localhost;database=common_db;uid=root;pwd=password;Allow User Variables=True;";
+            connection = new MySqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+        public int Insert(string name, string email, string phone, string comment_type, string msg)
+        {
+            try
+            {
+                int id = -1;
+                string query = "INSERT INTO gsm_contact (Name, Email_id, Phone, Request_type, Message, Status, Date) VALUES(@name, @email, @phone, @rtype, @msg, @sts, CURDATE())";
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@rtype", comment_type);
+                    cmd.Parameters.AddWithValue("@phone", phone);
+                    cmd.Parameters.AddWithValue("@msg", msg);
+                    cmd.Parameters.AddWithValue("@sts", false);
+
+                    cmd.ExecuteNonQuery();
+
+                    MySqlCommand cmd1 = new MySqlCommand("SELECT LAST_INSERT_ID()", connection);
+                    id = Convert.ToInt32(cmd1.ExecuteScalar());
+
+                    this.CloseConnection();
+                }
+                return id;
+            }
+            catch (MySqlException ex)
+            {
+                return -1;
+            }
+        }
+
+        public List<string>[] testimony_show(int offset)
+        {
+            try
+            {
+                //string query = "SELECT * FROM testimony ORDER BY Date DESC, ID DESC LIMIT 2 OFFSET @offset";
+                string query = "SELECT * FROM gsm_contact where Request_type='Feedback'";
+
+                list_feedback_show[0] = new List<string>();
+                list_feedback_show[1] = new List<string>();
+                list_feedback_show[2] = new List<string>();
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@offset", offset);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        list_feedback_show[0].Add(dataReader["Name"] + "");
+                        list_feedback_show[1].Add(dataReader["Message"] + "");
+                        list_feedback_show[2].Add(dataReader["Date"] + "");
+                    }
+
+                    dataReader.Close();
+                    this.CloseConnection();
+                    return list_feedback_show;
+                }
+                else
+                {
+                    return list_feedback_show;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return list_feedback_show;
+            }
+        }   
+
+    } //db_connect class
+} // namespace
