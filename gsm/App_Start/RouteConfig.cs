9d78609027153591a838cf9a13b9fa0e1da096d6
Vitthal Padwal <vitthalpadwal89@gmail.com>
2017-10-11 18:50:14 +0530

    modified client review comment

--- gsm/App_Start/RouteConfig.cs
+++ gsm/App_Start/RouteConfig.cs
@@ -17,6 +17,8 @@ namespace gsm
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               
+
             );
         }
     }
